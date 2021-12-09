786fa0d83b7c7173cca592c57cc8e4a4707b1abc
George Jahad <george@georgejahad.com>
2021-12-08 21:00:22 -0800

    fixed dist_dir

--- topologyAwareS3Poc/vars.sh
+++ topologyAwareS3Poc/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export OZONE_ROOT=$SCRIPT_DIR/hadoop-ozone/dist/target/ozone-1.3.0-SNAPSHOT
-DIST_DIR=$OZONE_ROOT/kubernetes/examples/ozone
+export DIST_DIR=$OZONE_ROOT/kubernetes/examples/ozone
 export DUMMY_BUCKET=dummy-bucket
 export DUMMY_FILE=dummy-file
 
