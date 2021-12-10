d80579615b3a2ba017032fd9cfb3aa18267379fb
George Jahad <george@georgejahad.com>
2021-12-09 21:41:00 -0800

    cleanup

--- topologyAwareS3Poc/setup.sh
+++ topologyAwareS3Poc/setup.sh
@@ -5,10 +5,14 @@ cd topologyAwareS3Poc
 git clone https://github.com/GeorgeJahad/ozone.git --branch topologyAwareS3Poc
 cd ozone/topologyAwareS3Poc
 chmod 755 *.sh
-wget https://dl.min.io/client/mc/release/linux-amd64/mc
-chmod +x mc
-wget https://github.com/elek/flekszible/releases/download/v2.3.0/flekszible_2.3.0_Linux_x86_64.tar.gz
-tar -xvf flekszible_2.3.0_Linux_x86_64.tar.gz
+
+if [[ `uname -s` == "Linux" ]] ; then
+    wget https://dl.min.io/client/mc/release/linux-amd64/mc
+    chmod +x mc
+    wget https://github.com/elek/flekszible/releases/download/v1.8.1/flekszible_1.8.1_Linux_x86_64.tar.gz
+    tar -xvf flekszible_1.8.1_Linux_x86_64.tar.gz
+fi
+
 CWD=`pwd`
 export PATH=$PATH:$CWD
 cd ..
@@ -16,6 +20,6 @@ echo building poc. build output at: `pwd`/topologyAwareS3Poc/pocBuild.txt
 mvn clean install -DskipShade -DskipTests >& topologyAwareS3Poc/pocBuild.txt
 echo starting kubernetes
 pocStart.sh
-pocLog.sh
+pocLog.sh &
 
 
