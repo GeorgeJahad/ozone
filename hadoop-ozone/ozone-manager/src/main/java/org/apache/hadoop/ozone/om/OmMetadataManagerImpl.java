99607ef2a7bbc7b66baef255cb13a08f1f690bd7
George Jahad <george@georgejahad.com>
2022-06-13 14:55:57 -0700

    made load read-only

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMetadataManagerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMetadataManagerImpl.java
@@ -282,7 +282,7 @@ public class OmMetadataManagerImpl implements OMMetadataManager {
  private OmMetadataManagerImpl(OzoneConfiguration conf, String snapshotName) throws IOException {
     lock = new OzoneManagerLock(conf);
     omEpoch = 0;
-    setStore(loadDB(conf, new File("/data/metadata"), snapshotName));
+    setStore(loadDB(conf, new File("/data/metadata"), snapshotName, true));
     initializeOmTables();
   }
 
@@ -419,15 +419,16 @@ public class OmMetadataManagerImpl implements OMMetadataManager {
 
   public static DBStore loadDB(OzoneConfiguration configuration, File metaDir)
       throws IOException {
-    return loadDB(configuration, metaDir, OM_DB_NAME);
+    return loadDB(configuration, metaDir, OM_DB_NAME, false);
   }
 
   public static DBStore loadDB(OzoneConfiguration configuration, File metaDir,
-      String dbName) throws IOException {
+      String dbName, boolean readOnly) throws IOException {
     RocksDBConfiguration rocksDBConfiguration =
         configuration.getObject(RocksDBConfiguration.class);
     DBStoreBuilder dbStoreBuilder = DBStoreBuilder.newBuilder(configuration,
         rocksDBConfiguration).setName(dbName)
+        .setOpenReadOnly(readOnly)
         .setPath(Paths.get(metaDir.getPath()));
     DBStore dbStore = addOMTablesAndCodecs(dbStoreBuilder).build();
     return dbStore;
