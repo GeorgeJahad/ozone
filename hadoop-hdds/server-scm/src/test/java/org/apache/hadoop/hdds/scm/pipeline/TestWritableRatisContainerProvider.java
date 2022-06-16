4a607fde1079518d137dd2e2d348e03f61b2f1fd
George Jahad <george@georgejahad.com>
2022-06-16 16:39:39 -0700

    working

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/pipeline/TestWritableRatisContainerProvider.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/pipeline/TestWritableRatisContainerProvider.java
@@ -48,8 +48,7 @@ import java.io.IOException;
 import java.util.*;
 
 import static org.apache.hadoop.hdds.conf.StorageUnit.BYTES;
-import static org.apache.hadoop.hdds.scm.pipeline.Pipeline.PipelineState.CLOSED;
-import static org.apache.hadoop.hdds.scm.pipeline.Pipeline.PipelineState.OPEN;
+import static org.apache.hadoop.hdds.scm.pipeline.Pipeline.PipelineState.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -61,7 +60,7 @@ public class TestWritableRatisContainerProvider {
   private static final Logger LOG = LoggerFactory
       .getLogger(TestWritableRatisContainerProvider.class);
   private static final String OWNER = "SCM";
-  private PipelineManager pipelineManager;
+  private PipelineManager pipelineManager, pipelineManagerSpy;
   private ContainerManager containerManager
       = Mockito.mock(ContainerManager.class);
   private PipelineChoosePolicy pipelineChoosingPolicy
@@ -74,15 +73,13 @@ public class TestWritableRatisContainerProvider {
   private WritableContainerProvider provider;
   private ReplicationConfig repConfig;
   private int minPipelines;
+  private Pipeline allocatedPipeline;
 
   private Map<ContainerID, ContainerInfo> containers;
 
-  @BeforeEach
   public void setup() throws IOException {
     repConfig = RatisReplicationConfig.getInstance(HddsProtos.ReplicationFactor.ONE);
     conf = new OzoneConfiguration();
-    minPipelines = 5; //?
-    containers = new HashMap<>();
     File testDir = GenericTestUtils.getTestDir(
         TestContainerManagerImpl.class.getSimpleName() + UUID.randomUUID());
     conf.set(HddsConfigKeys.OZONE_METADATA_DIRS, testDir.getAbsolutePath());
@@ -92,322 +89,46 @@ public class TestWritableRatisContainerProvider {
     nodeManager = new MockNodeManager(true, 10);
     pipelineManager =
         new MockPipelineManager(dbStore, scmhaManager, nodeManager);
-    PipelineManager pipelineManagerSpy = spy(pipelineManager);
-    doThrow(new SCMException(SCMException.ResultCodes.IO_EXCEPTION))
-        .when(pipelineManagerSpy).waitPipelineReady(any(), anyLong());
+    pipelineManagerSpy = spy(pipelineManager);
+    doThrow(SCMException.class).when(pipelineManagerSpy).createPipeline(any(), any(), anyList());
     provider = new WritableRatisContainerProvider(
         conf, pipelineManagerSpy, containerManager, pipelineChoosingPolicy);
-    Pipeline allocatedPipeline = pipelineManager.createPipeline(repConfig);
+    allocatedPipeline = pipelineManager.createPipeline(repConfig);
     ((MockPipelineManager)pipelineManager).allocatePipeline(allocatedPipeline, false);
+    ContainerInfo container = createContainer(allocatedPipeline,
+        repConfig, System.nanoTime());
+    pipelineManager.addContainerToPipeline(
+        allocatedPipeline.getId(), container.containerID());
 
-    Mockito.doAnswer(call -> {
-      Pipeline pipeline = (Pipeline)call.getArguments()[2];
-      ContainerInfo container = createContainer(pipeline,
-          repConfig, System.nanoTime());
-      pipelineManager.addContainerToPipeline(
-          pipeline.getId(), container.containerID());
-      containers.put(container.containerID(), container);
-      return container;
-    }).when(containerManager).getMatchingContainer(Matchers.anyLong(),
-        Matchers.anyString(), Matchers.any(Pipeline.class));
+    doReturn(container).when(containerManager).getMatchingContainer(anyLong(),
+        anyString(), eq(allocatedPipeline), any());
 
-    Mockito.doAnswer(call ->
-        containers.get((ContainerID)call.getArguments()[0]))
-        .when(containerManager).getContainer(Matchers.any(ContainerID.class));
   }
 
   @Test
   public void testWaitForAllocatedPipeline()
       throws IOException {
-
-    provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-
-    
-    // The first 5 calls should return a different container
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container =
-          provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-      assertFalse(allocatedContainers.contains(container));
-      allocatedContainers.add(container);
-    }
-
-    allocatedContainers.clear();
-    for (int i = 0; i < 20; i++) {
-      ContainerInfo container =
-          provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-      allocatedContainers.add(container);
-    }
-    // Should have minPipelines containers created
-    assertEquals(minPipelines,
-        pipelineManager.getPipelines(repConfig, OPEN).size());
-    // We should have more than 1 allocatedContainers in the set proving a
-    // random container is selected each time. Do not check for 5 here as there
-    // is a reasonable chance that in 20 turns we don't pick all 5 nodes.
-    assertTrue(allocatedContainers.size() > 2);
-  }
-
-  @Test
-  public void testPiplineLimitIgnoresExcludedPipelines() throws IOException {
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container = provider.getContainer(
-          1, repConfig, OWNER, new ExcludeList());
-      allocatedContainers.add(container);
-    }
-    // We have the min limit of pipelines, but then exclude one. It should use
-    // one of the existing rather than createing a new one, as the limit is
-    // checked against all pipelines, not just the filtered list
-    ExcludeList exclude = new ExcludeList();
-    PipelineID excludedID = allocatedContainers
-        .stream().findFirst().get().getPipelineID();
-    exclude.addPipeline(excludedID);
-
-    ContainerInfo c = provider.getContainer(1, repConfig, OWNER, exclude);
-    assertNotEquals(excludedID, c.getPipelineID());
-    assertTrue(allocatedContainers.contains(c));
-  }
-
-  @Test
-  public void testNewPipelineCreatedIfAllPipelinesExcluded()
-      throws IOException {
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container = provider.getContainer(
-          1, repConfig, OWNER, new ExcludeList());
-      allocatedContainers.add(container);
-    }
-    // We have the min limit of pipelines, but then exclude one. It should use
-    // one of the existing rather than creating a new one, as the limit is
-    // checked against all pipelines, not just the filtered list
-    ExcludeList exclude = new ExcludeList();
-    for (ContainerInfo c : allocatedContainers) {
-      exclude.addPipeline(c.getPipelineID());
-    }
-    ContainerInfo c = provider.getContainer(1, repConfig, OWNER, exclude);
-    assertFalse(allocatedContainers.contains(c));
-  }
-
-  @Test
-  public void testNewPipelineCreatedIfAllContainersExcluded()
-      throws IOException {
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container = provider.getContainer(
-          1, repConfig, OWNER, new ExcludeList());
-      allocatedContainers.add(container);
-    }
-    // We have the min limit of pipelines, but then exclude one. It should use
-    // one of the existing rather than createing a new one, as the limit is
-    // checked against all pipelines, not just the filtered list
-    ExcludeList exclude = new ExcludeList();
-    for (ContainerInfo c : allocatedContainers) {
-      exclude.addConatinerId(c.containerID());
-    }
-    ContainerInfo c = provider.getContainer(1, repConfig, OWNER, exclude);
-    assertFalse(allocatedContainers.contains(c));
-  }
-
-  @Test
-  public void testUnableToCreateAnyPipelinesReturnsNull() throws IOException {
-    pipelineManager = new MockPipelineManager(
-        dbStore, scmhaManager, nodeManager) {
-      @Override
-      public Pipeline createPipeline(ReplicationConfig repConf,
-          List<DatanodeDetails> excludedNodes,
-          List<DatanodeDetails> favoredNodes) throws IOException {
-        throw new IOException("Cannot create pipelines");
-      }
-    };
-    provider = new WritableRatisContainerProvider(
-        conf, pipelineManager, containerManager, pipelineChoosingPolicy);
+    setup();
+    assertTrue(pipelineManager.getPipelines(repConfig, OPEN).isEmpty());
+    assertTrue(pipelineManager.getPipelines(repConfig, ALLOCATED)
+        .contains(allocatedPipeline));
 
     ContainerInfo container =
         provider.getContainer(1, repConfig, OWNER, new ExcludeList());
     assertNull(container);
-  }
 
-  @Test
-  public void testExistingPipelineReturnedWhenNewCannotBeCreated()
-      throws IOException {
-    pipelineManager = new MockPipelineManager(
-        dbStore, scmhaManager, nodeManager) {
+    setup();
+    assertTrue(pipelineManager.getPipelines(repConfig, OPEN).isEmpty());
+    assertTrue(pipelineManager.getPipelines(repConfig,  ALLOCATED).contains(allocatedPipeline));
 
-      private boolean throwError = false;
-
-      @Override
-      public Pipeline createPipeline(ReplicationConfig repConf,
-          List<DatanodeDetails> excludedNodes,
-          List<DatanodeDetails> favoredNodes) throws IOException {
-        if (throwError) {
-          throw new IOException("Cannot create pipelines");
-        }
-        throwError = true;
-        return super.createPipeline(repConfig);
-      }
-    };
-    provider = new WritableRatisContainerProvider(
-        conf, pipelineManager, containerManager, pipelineChoosingPolicy);
-
-    ContainerInfo container =
-        provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-    for (int i = 0; i < 5; i++) {
-      ContainerInfo nextContainer =
-          provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-      assertEquals(container, nextContainer);
-    }
-  }
-
-  @Test
-  public void testNewContainerAllocatedAndPipelinesClosedIfNoSpaceInExisting()
-      throws IOException {
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container =
-          provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-      assertFalse(allocatedContainers.contains(container));
-      allocatedContainers.add(container);
-    }
-    // Update all the containers to make them nearly full, but with enough space
-    // for an EC block to be striped across them.
-    for (ContainerInfo c : allocatedContainers) {
-      c.setUsedBytes(getMaxContainerSize() - 30 * 1024 * 1024);
-    }
-
-    // Get a new container of size 50 and ensure it is one of the original set.
-    // We ask for a space of 50, but as it is stripped across the EC group it
-    // will actually need 50 / dataNum space
-    ContainerInfo newContainer =
-        provider.getContainer(50 * 1024 * 1024, repConfig, OWNER,
-            new ExcludeList());
-    assertNotNull(newContainer);
-    assertTrue(allocatedContainers.contains(newContainer));
-    // Now get a new container where there is not enough space in the existing
-    // and ensure a new container gets created.
-    newContainer = provider.getContainer(
-        128 * 1024 * 1024, repConfig, OWNER, new ExcludeList());
-    assertNotNull(newContainer);
-    assertFalse(allocatedContainers.contains(newContainer));
-    // The original pipelines should all be closed, triggered by the lack of
-    // space.
-    for (ContainerInfo c : allocatedContainers) {
-      Pipeline pipeline = pipelineManager.getPipeline(c.getPipelineID());
-      assertEquals(CLOSED, pipeline.getPipelineState());
-    }
-  }
-
-  @Test
-  public void testPipelineNotFoundWhenAttemptingToUseExisting()
-      throws IOException {
-    // Ensure PM throws PNF exception when we ask for the containers in the
-    // pipeline
-    pipelineManager = new MockPipelineManager(
-        dbStore, scmhaManager, nodeManager) {
-
-      @Override
-      public NavigableSet<ContainerID> getContainersInPipeline(
-          PipelineID pipelineID) throws IOException {
-        throw new PipelineNotFoundException("Simulated exception");
-      }
-    };
-    provider = new WritableRatisContainerProvider(
-        conf, pipelineManager, containerManager, pipelineChoosingPolicy);
-
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container =
-          provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-      assertFalse(allocatedContainers.contains(container));
-      allocatedContainers.add(container);
-    }
-    // Now attempt to get a container - any attempt to use an existing with
-    // throw PNF and then we must allocate a new one
-    ContainerInfo newContainer =
-        provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-    assertNotNull(newContainer);
-    assertFalse(allocatedContainers.contains(newContainer));
-  }
-
-  @Test
-  public void testContainerNotFoundWhenAttemptingToUseExisting()
-      throws IOException {
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container =
-          provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-      assertFalse(allocatedContainers.contains(container));
-      allocatedContainers.add(container);
-    }
-
-    // Ensure ContainerManager always throws when a container is requested so
-    // existing pipelines cannot be used
-    Mockito.doAnswer(call -> {
-      throw new ContainerNotFoundException();
-    }).when(containerManager).getContainer(Matchers.any(ContainerID.class));
-
-    ContainerInfo newContainer =
-        provider.getContainer(1, repConfig, OWNER, new ExcludeList());
-    assertNotNull(newContainer);
-    assertFalse(allocatedContainers.contains(newContainer));
-
-    // Ensure all the existing pipelines are closed
-    for (ContainerInfo c : allocatedContainers) {
-      Pipeline pipeline = pipelineManager.getPipeline(c.getPipelineID());
-      assertEquals(CLOSED, pipeline.getPipelineState());
-    }
-  }
-
-  @Test
-  public void testPipelineOpenButContainerRemovedFromIt() throws IOException {
-    // This can happen if the container close process is triggered from the DN.
-    // When tha happens, CM will change the container state to CLOSING and
-    // remove it from the container list in pipeline Manager.
-    Set<ContainerInfo> allocatedContainers = new HashSet<>();
-    for (int i = 0; i < minPipelines; i++) {
-      ContainerInfo container = provider.getContainer(
-          1, repConfig, OWNER, new ExcludeList());
-      assertFalse(allocatedContainers.contains(container));
-      allocatedContainers.add(container);
-      // Remove the container from the pipeline to simulate closing it
-      pipelineManager.removeContainerFromPipeline(
-          container.getPipelineID(), container.containerID());
-    }
-    ContainerInfo newContainer = provider.getContainer(
-        1, repConfig, OWNER, new ExcludeList());
-    assertFalse(allocatedContainers.contains(newContainer));
-    for (ContainerInfo c : allocatedContainers) {
-      Pipeline pipeline = pipelineManager.getPipeline(c.getPipelineID());
-      assertEquals(CLOSED, pipeline.getPipelineState());
-    }
-  }
-
-  @Test
-  public void testExcludedNodesPassedToCreatePipelineIfProvided()
-      throws IOException {
-    PipelineManager pipelineManagerSpy = spy(pipelineManager);
-    provider = new WritableRatisContainerProvider(
-        conf, pipelineManagerSpy, containerManager, pipelineChoosingPolicy);
-    ExcludeList excludeList = new ExcludeList();
-
-    // EmptyList should be passed if there are no nodes excluded.
-    ContainerInfo container = provider.getContainer(
-        1, repConfig, OWNER, excludeList);
+    doAnswer(call -> {
+      pipelineManager.openPipeline(allocatedPipeline.getId());
+      return allocatedPipeline;
+    }).when(pipelineManagerSpy).waitOnePipelineReady(any(), anyLong());
+    container = provider.getContainer(1, repConfig, OWNER, new ExcludeList());
     assertNotNull(container);
 
-    verify(pipelineManagerSpy).createPipeline(repConfig,
-        Collections.emptyList(), Collections.emptyList());
 
-    // If nodes are excluded then the excluded nodes should be passed through to
-    // the create pipeline call.
-    excludeList.addDatanode(MockDatanodeDetails.randomDatanodeDetails());
-    List<DatanodeDetails> excludedNodes =
-        new ArrayList<>(excludeList.getDatanodes());
-
-    container = provider.getContainer(
-        1, repConfig, OWNER, excludeList);
-    assertNotNull(container);
-    verify(pipelineManagerSpy).createPipeline(repConfig, excludedNodes,
-        Collections.emptyList());
   }
 
   private ContainerInfo createContainer(Pipeline pipeline,
@@ -423,12 +144,6 @@ public class TestWritableRatisContainerProvider {
         .setSequenceId(0)
         .setDeleteTransactionId(0)
         .build();
-  }
-
-  private long getMaxContainerSize() {
-    return (long)conf.getStorageSize(
-        ScmConfigKeys.OZONE_SCM_CONTAINER_SIZE,
-        ScmConfigKeys.OZONE_SCM_CONTAINER_SIZE_DEFAULT, BYTES);
   }
 
 }
