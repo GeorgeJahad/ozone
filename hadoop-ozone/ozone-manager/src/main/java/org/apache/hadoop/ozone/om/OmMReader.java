dd01353137322009edba4e20f07f697ff22d2a82
George Jahad <george@georgejahad.com>
2022-08-01 13:08:55 -0700

    checkstyle

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMReader.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMReader.java
@@ -17,15 +17,9 @@
 
 package org.apache.hadoop.ozone.om;
 
-import org.apache.hadoop.hdds.utils.db.DBCheckpoint;
-import org.apache.hadoop.hdds.utils.db.RDBStore;
-
 import java.io.IOException;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.hadoop.hdds.StringUtils;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
-import org.apache.hadoop.hdds.scm.protocol.ScmBlockLocationProtocol;
-import org.apache.hadoop.hdds.scm.protocol.StorageContainerLocationProtocol;
 import org.apache.hadoop.ipc.ProtobufRpcEngine;
 import org.apache.hadoop.ipc.Server;
 import org.apache.hadoop.ozone.OzoneAcl;
@@ -43,31 +37,21 @@ import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
 import org.apache.hadoop.ozone.om.helpers.OmVolumeArgs;
 import org.apache.hadoop.ozone.om.helpers.OzoneFileStatus;
 import org.apache.hadoop.ozone.om.request.OMClientRequest;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.KeyArgs;
 import org.apache.hadoop.ozone.security.acl.OzoneObjInfo;
 import org.apache.hadoop.ozone.security.acl.RequestContext;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import static org.apache.hadoop.hdds.server.ServerUtils.getRemoteUserName;
-import static org.apache.hadoop.hdds.utils.HAUtils.getScmBlockClient;
-import static org.apache.hadoop.hdds.utils.HAUtils.getScmContainerClient;
 import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS;
 import static org.apache.hadoop.ozone.om.KeyManagerImpl.getRemoteUser;
-import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_OM_VOLUME_LISTALL_ALLOWED;
-import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_OM_VOLUME_LISTALL_ALLOWED_DEFAULT;
 import static org.apache.hadoop.ozone.om.OzoneManager.getS3Auth;
 import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes.DETECTED_LOOP_IN_BUCKET_LINKS;
 import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes.INVALID_REQUEST;
@@ -82,10 +66,6 @@ import org.apache.hadoop.ozone.security.acl.OzoneObj;
 import org.apache.hadoop.ozone.security.acl.OzoneObj.ResourceType;
 import org.apache.hadoop.ozone.security.acl.OzoneObj.StoreType;
 import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes;
-import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes.INVALID_AUTH_METHOD;
-import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes.INVALID_REQUEST;
-import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes.PERMISSION_DENIED;
-import static org.apache.hadoop.ozone.om.exceptions.OMException.ResultCodes.TOKEN_ERROR_OTHER;
 
 /**
  * OM Metadata Reading class for the OM and Snapshot managers.
@@ -97,10 +77,8 @@ public class OmMReader implements Auditor {
   private final BucketManager bucketManager;
   private final OMMetadataManager metadataManager;
   private final OzoneManager ozoneManager;
-  private final OzoneConfiguration configuration;
   private final boolean isAclEnabled;
   private final IAccessAuthorizer accessAuthorizer;
-  private final boolean allowListAllVolumes;
   private final boolean isNativeAuthorizerEnabled;
   private final OmMReaderMetrics metrics;
   private final Logger log;
@@ -118,12 +96,12 @@ public class OmMReader implements Auditor {
     this.volumeManager = ozoneManager.getVolumeManager();
     this.prefixManager = prefixManager;
     this.metadataManager = metadataManager;
-    this.configuration = ozoneManager.getConfiguration();
+    OzoneConfiguration configuration = ozoneManager.getConfiguration();
     this.ozoneManager = ozoneManager;
     this.isAclEnabled = ozoneManager.getAclsEnabled();
     this.log = log;
     this.audit = audit;
-    this.allowListAllVolumes = ozoneManager.getAllowListAllVolumes();
+    boolean allowListAllVolumes = ozoneManager.getAllowListAllVolumes();
     metrics = omMReaderMetrics;
     if (isAclEnabled) {
       accessAuthorizer = getACLAuthorizerInstance(configuration);
@@ -184,7 +162,7 @@ public class OmMReader implements Auditor {
   }
 
   public List<OzoneFileStatus> listStatus(OmKeyArgs args, boolean recursive,
-                                          String startKey, long numEntries, boolean allowPartialPrefixes)
+      String startKey, long numEntries, boolean allowPartialPrefixes)
       throws IOException {
 
     ResolvedBucket bucket = resolveBucketLink(args);
@@ -383,7 +361,7 @@ public class OmMReader implements Auditor {
    * @param volumeAndBucket the bucket to be resolved (if it is a link)
    * @param visited collects link buckets visited during the resolution to
    *   avoid infinite loops
-   * @param {@link UserGroupInformation}
+   * @param userGroupInformation {@link UserGroupInformation}
    * @param remoteAddress
    * @param hostName
    * @return bucket location possibly updated with its actual volume and bucket
@@ -653,7 +631,7 @@ public class OmMReader implements Auditor {
   }
 
   public AuditMessage buildAuditMessageForSuccess(AuditAction op,
-                                                  Map<String, String> auditMap) {
+      Map<String, String> auditMap) {
 
     return new AuditMessage.Builder()
         .setUser(getRemoteUserName())
@@ -675,12 +653,6 @@ public class OmMReader implements Auditor {
         .withResult(AuditEventStatus.FAILURE)
         .withException(throwable)
         .build();
-  }
-
-  private Map<String, String> buildAuditMap(String volume) {
-    Map<String, String> auditMap = new LinkedHashMap<>();
-    auditMap.put(OzoneConsts.VOLUME, volume);
-    return auditMap;
   }
 
   /**
