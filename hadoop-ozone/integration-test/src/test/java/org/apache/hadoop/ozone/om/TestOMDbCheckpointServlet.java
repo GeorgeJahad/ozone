15d451742a480c6baa39634e02cede16f4590fb3
George Jahad <george@georgejahad.com>
2023-06-20 08:45:21 -0700

    force failure and then fix

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestOMDbCheckpointServlet.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestOMDbCheckpointServlet.java
@@ -517,12 +517,26 @@ public class TestOMDbCheckpointServlet {
         .getClientProxy().getOzoneManagerClient();
 
     writeClient.createSnapshot(vname, bname, snapshotName);
+
+    //Thread.sleep(10000);
+    om.getOmRatisServer().getOmStateMachine().awaitDoubleBufferFlush();
+
+    // DBCheckpoint checkpoint = null;
+    // checkpoint = om.getMetadataManager().getStore().getCheckpoint(true);
+    // OmMetadataManagerImpl checkpointMetadataManager =
+    //     OmMetadataManagerImpl.createCheckpointMetadataManager(
+    //         conf, checkpoint);
+    // SnapshotInfo checkpointSnapshotInfo = checkpointMetadataManager.getSnapshotInfoTable()
+    //     .get(SnapshotInfo.getTableKey(vname, bname, snapshotName));
+    // Assert.assertNotNull("checkoint snapshot info should exist",
+    //     checkpointSnapshotInfo);
+    // checkpointMetadataManager.stop();
     SnapshotInfo snapshotInfo = om.getMetadataManager().getSnapshotInfoTable()
         .get(SnapshotInfo.getTableKey(vname, bname, snapshotName));
     String snapshotPath = getSnapshotPath(conf, snapshotInfo)
         + OM_KEY_PREFIX;
     GenericTestUtils.waitFor(() -> new File(snapshotPath).exists(),
-        100, 2000);
+        100, 20000);
     return snapshotPath;
   }
 
